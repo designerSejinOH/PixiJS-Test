14d85275d2d6a9070bddafb1040cee65ba742183
SejinOH <39869184+designerSejinOH@users.noreply.github.com>
2023-04-05 03:52:17 +0900

    Fix

--- client/src/components/canvas/Scene.tsx
+++ client/src/components/canvas/Scene.tsx
@@ -1,15 +1,59 @@
-import { BlurFilter } from "pixi.js";
-import { Stage, Container, Sprite, Text } from "@pixi/react";
+// @ts-nocheck
+import { Stage, Sprite } from "@pixi/react";
+import * as PIXI from "pixi.js";
+import React from "react";
+
+interface Draggable extends PIXI.DisplayObject {
+  data: PIXI.InteractionData | null;
+  dragging: boolean;
+}
 
 export const Scene = ({ width, height }) => {
+  const onDragStart = (event: PIXI.InteractionEvent) => {
+    const sprite = event.currentTarget as Draggable;
+    sprite.alpha = 0.5;
+    sprite.data = event.data;
+    sprite.dragging = true;
+  };
+
+  const onDragEnd = (event: PIXI.InteractionEvent) => {
+    const sprite = event.currentTarget as Draggable;
+    sprite.alpha = 1;
+    sprite.dragging = false;
+    sprite.data = null;
+  };
+
+  const onDragMove = (event: PIXI.InteractionEvent) => {
+    const sprite = event.currentTarget as Draggable;
+    if (sprite.dragging) {
+      const newPosition = sprite.data!.getLocalPosition(sprite.parent);
+      sprite.x = newPosition.x;
+      sprite.y = newPosition.y;
+    }
+  };
+
   return (
     <Stage width={width} height={height}>
       <Sprite
-        image="https://pixijs.io/pixi-react/img/bunny.png"
+        image="./img/nike.png"
+        x={width / 2}
+        y={height / 2}
+        scale={0.1}
+        anchor={0.5}
+        interactive
+        buttonMode
+        pointerdown={onDragStart}
+        pointerup={onDragEnd}
+        pointerupoutside={onDragEnd}
+        pointermove={onDragMove}
+      />
+      {/* <Sprite
+        image="./img/nike.png"
+        scale={0.2}
         x={width / 2}
         y={height / 2}
         anchor={{ x: 0.5, y: 0.5 }}
-      />
+      /> */}
     </Stage>
   );
 };
--- client/src/components/dom/Header.tsx
+++ client/src/components/dom/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = ({ title, info }) => {
   return (
-    <div className="w-full fixed h-20 border border-black bg-white bg-opacity-50 px-10 rounded-full flex flex-row justify-between items-center">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <h1 className="">{info}</h1>
+    <div className="w-[95vw] my-2 fixed py-2 px-4 h-fit text-white bg-white bg-opacity-50 rounded-full flex flex-row justify-between items-center">
+      <h1 className="text-2xl font-light">{title}</h1>
+      <h1 className="text-2xl font-bold">{info}</h1>
     </div>
   );
 };
